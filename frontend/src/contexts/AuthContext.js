5d647939d3519aa55af70fad8f9a15b84c9d031d
jrusi <quinametin@gmail.com>
2025-05-21 07:48:03 +0200

    added username to authcontext

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -73,6 +73,7 @@ export const AuthProvider = ({ children }) => {
   const register = async (email, password, fullName) => {
     try {
       await axios.post('/users/', {
+        username,
         email,
         password,
         full_name: fullName
